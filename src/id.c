43efa60930313d94dd9d5f73a5aff7e768099d5b
Gabriel Lopes Veiga <gabriellopvei@gmail.com>
2021-08-10 18:40:29 -0300

    improve id gen
    was very bad

--- src/id.c
+++ src/id.c
@@ -5,20 +5,20 @@ unsigned long current_id = 0;
 long id_get() { return ++current_id; }
 
 char ctochar(unsigned char c) {
-  int numberity = !(c < 10);
+  int isalpha = c > 9;
 
   // If < 10, is number, otherwise lowercase letter
-  return c + 48 + (39 * numberity);
+  return c + 48 + (39 * isalpha);
 }
 
 void id_get_str(char *dest) {
-  const int max = 29;
-  unsigned long base = id_get() % max;
-  unsigned char c = base;
+  const int max = 36;
+  const int magic = 0x3e779921;
+  unsigned long base = id_get();
+  unsigned long c = base;
 
   for (int i = 0; i < ID_STR_LENGTH; i++) {
-    // ((c + 1) * 33) % max
-    c = (((c + i) << 5) + c) % max;
-    dest[i] = ctochar(c);
+    c ^= base + magic + (c << 6) + (c >> 2);
+    dest[i] = ctochar(c % max);
   }
 }
--- src/id.h
+++ src/id.h
@@ -1,7 +1,7 @@
 #ifndef H_PASS_OR_REPASS_ID
 #define H_PASS_OR_REPASS_ID
 
-#define ID_STR_LENGTH 64
+#define ID_STR_LENGTH 8
 
 long id_get();
 void id_get_str(char *dest);
